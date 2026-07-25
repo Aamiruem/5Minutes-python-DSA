0b71f47403b6e41edcf3350ede867c17ab49a6d2
Aamir Hussain <aamirhussainazad786@gmail.com>
2026-07-25 18:19:12 +0530

    commit add

--- Lecture15 to 16/Session 15&16/dsapro1/analyzer.py
+++
@@ -1,7 +0,0 @@
-class RandomAnalyzer:
-    def __init__(self, numbers):
-        self.numbers = numbers
-
-    def to_dataframe(self):
-        import pandas as pd
-        return pd.DataFrame({"Random Numbers": self.numbers})
